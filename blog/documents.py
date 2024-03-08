cda0d69bd814b9e90847f74a6c215c25fa22d921
GloryKO <koladeg2022@gmail.com>
2024-03-08 18:58:33 +0100

    added article document with relations

--- blog/documents.py
+++ blog/documents.py
@@ -15,3 +15,44 @@ class UserDocument(Document):
         model =User
         fields=["id","first_name","last_name","username"]
 
+@registry.register_document
+class CategoryDocument(Document):
+    id =fields.IntegerField()
+    class Index:
+        name="categories"
+        settings ={
+            "number_of_shards":1,
+            "number_of_replicas":0,
+        }
+    class Django:
+        model =Category
+        fields = ["name","description",]
+    
+@registry.register_document
+class ArticleDocument(Document): #define the foreign keys (author and categories)
+    author = fields.ObjectField(properties={
+        "id":fields.IntegerField(),
+        "first_name":fields.TextField(),
+        "last_name":fields.TextField(),
+        "user_name":fields.TextField(),
+
+    })
+    categories = fields.ObjectField(properties={
+        "id":fields.IntegerField(),
+        "name":fields.TextField(),
+        "description":fields.TextField()
+    })
+    type = fields.TextField(attr="type_to_string")
+    class Index:
+        name ="aticle"
+        settings ={
+            "number_of_shards":1,
+            "number_of_replica":0,
+        }
+    class Django:
+        model = Article
+        fields=[ "title",
+            "content",
+            "created_datetime",
+            "updated_datetime"
+            ]
